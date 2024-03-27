2f9e98675670fc9d11f0ee2e2b709a584b675723
DoHyeonHak <ehgusgkr6@naver.com>
2024-03-27 20:05:28 +0900

    make deleteContact

--- lab4/C006.c
+++ lab4/C006.c
@@ -129,7 +129,31 @@ int addContact(Contact* c[], int size){
 }
 
 int deleteContact(Contact* c[], int size){
+	int number = 0;
+	int yon = 0;
+	printf("> Delete Contact\n");
+	printf("> Enter a number of Contact : ");
+	scanf("%d", &number);
+	if(number - 1 < size){
+		printf("> Contact Information\n");
+		printf("[%2d] %s : %s | %s | %s | %s\n", number, c[number - 1]->name, c[number - 1]->phone_num, c[number - 1]->email, c[number - 1]->relationship, sign[c[number - 1]->favorite]);
+	}else{
+		printf("> Wrong number\n");
+		return size;
+	}
+	
+	printf("> If you want to delete this contact? (yes = 1, no = 0)\n");
+	scanf("%d", &yon);
 
+	if(yon == 1){
+		for(int i = number - 1; i < size; i ++){
+			c[i] = c[i + 1];
+		}
+		printf("Contact is deleted.\n");
+		return size - 1;
+	}else{
+		printf("Canceled\n");
+	}
 	return size;
 }
 
