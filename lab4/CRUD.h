9d81210a4f3b9066cc57fe4e3ddfc978a2501066
DoHyeonHak <ehgusgkr6@naver.com>
2024-03-29 15:05:05 +0900

    [lab]

---
+++ lab4/CRUD.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "Contact.h"
+
+int addContact(Contact* c[], int size);
+void printContact(Contact* c[], int size);
+void editContact(Contact* c[], int size);
+int deleteContact(Contact* c[], int size);
