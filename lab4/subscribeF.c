b0fe29a2387ebe813332f74a56f214bd1af2b134
DoHyeonHak <ehgusgkr6@naver.com>
2024-03-29 13:48:17 +0900

    modify subscribeF.c

--- lab4/subscribeF.c
+++ lab4/subscribeF.c
@@ -14,12 +14,13 @@ void subscribeFavorite(Contact* c[], int size){
 			printf("[%2d] %-8s : %s | %-23s | %-10s\n", i + 1, c[i]->name, c[i]->phone_num, c[i]->email, c[i]->relationship);
 		}
 	}
-
 	// read num
 	// if contact is not favorite
 	//		if yon is 1, subscribe. else pass
 	// if contact is favorite
 	//		if yon is 1, unsubscribe. else pass 
+
+
 	printf("\n> Enter Contact number: ");
 	scanf("%d", &num);
 	if(c[num - 1]->favorite == 0){
@@ -39,5 +40,12 @@ void subscribeFavorite(Contact* c[], int size){
 			printf("> Unsubscribed!\n");
 		}
 	}
-
+	if(yon == 1){
+		printf("\n> Modified Favorite Contact List\n");				// for checking Favorite Contact List
+		for(int i = 0; i < size; i ++){
+			if(c[i]->favorite == 1){
+				printf("[%2d] %-8s : %s | %-23s | %-10s\n", i + 1, c[i]->name, c[i]->phone_num, c[i]->email, c[i]->relationship);
+			}
+		}
+	}
 }
